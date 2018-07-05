fef5ff77d8d3631739af1876c71676e2224b5045
Jonathan Kim <jonkim204@gmail.com>
2018-07-04 20:09:46 -0700

    finished knex

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,7 +2,7 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+let searchTerm;
 knex
   .select('notes.id', 'title', 'content')
   .from('notes')
@@ -18,3 +18,65 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+knex
+  .select('id', 'title', 'content')
+  .from('notes')
+  .where('id', 1000)
+  
+  .then(([results]) => {
+    if (results) {
+      console.log(JSON.stringify(results, null, 2));
+    }else{
+      console.log('Not Found');
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });  
+
+let updateInfo = {title: 'test', content:'stuff'};
+knex
+  .update(updateInfo)
+  .from('notes')
+  .where('id', 1002)
+  .returning(['id', 'title', 'content'])
+  
+  .then(([results]) => {
+    if (results) {
+      console.log(JSON.stringify(results, null, 2));
+    }else{
+      console.log('Not Found');
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });  
+
+let newInfo = {title: 'NEW', content: 'STUFF'};
+knex
+  .insert(newInfo)
+  .from('notes')
+  .returning(['id', 'title', 'content'])
+  
+  .then(([results]) => {
+    if (results) {
+      console.log(JSON.stringify(results, null, 2));
+    }else{
+      console.log('Not Found');
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });  
+
+knex
+  .where('id', 1003)
+  .from('notes')
+  .del()
+  .then(() => {
+    console.log('Deleted');
+  })
+  .catch(err => {
+    console.error(err);
+  });  
